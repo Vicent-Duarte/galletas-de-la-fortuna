96eb2bd56d96f55fecbabf223627396b8fe6ee96
Vicent Duarte <vicentduarte70@gmail.com>
2024-03-30 21:18:03 -0400

    Galletas de la Fortuna

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css';
+import arrayPhrases from './utils/phrases.json';
+import getRandom from './services/getRandom.js';
+import ShowPhrase from './components/ShowPhrase.jsx';
+import arrayImages from './utils/images.json'
+import PhraseButton from './components/PhraseButton.jsx';
+
+
+
+
+function App  () {
+  
+  const [text, setPhrase] = useState(getRandom(arrayPhrases));
+  const [image, setImage] = useState(getRandom(arrayImages));
+
+
+  const objStyles = {
+    backgroundImage: `url(../public/Fortuna-2/fondo${image}.png)`,
+  }
+
+  return (
+    <div className='app' style={objStyles}>
+      <h1 className='app__title'>Galletas de la Fortuna</h1>
+      
+      <PhraseButton
+        setPhrase={setPhrase}
+        setImage={setImage}
+      />
+      <ShowPhrase
+        phrase={text}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/PhraseButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import arrayPhrases from '../utils/phrases.json';
+import getRandom from '../services/getRandom';
+import arrayImages from '../utils/images.json'
+
+const PhraseButton = ({setPhrase, setImage}) => {
+
+    const handlePhrase = () => {
+        setPhrase(getRandom(arrayPhrases));
+        setImage(getRandom(arrayImages));
+    }
+
+  return (
+    <button onClick={handlePhrase} className='btn'>Probar mi Suerte</button>
+  )
+}
+
+export default PhraseButton
